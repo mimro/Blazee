4df4d6559a0f6d9ad1b75e97c381ce1f0f77a093
Michał Mroczek <01036794@pw.edu.pl>
2022-02-13 22:03:51 +0100

    first matlab test

---
+++ Blazee.CLI/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Blazee.MatLab;
+
+var i = new ImageToBinary();
+i.Process(null);
+Console.WriteLine("Hello, World!");
---
+++ Blazee.MatLab/ImageToBinary.cs
@@ -0,0 +1,13 @@
+﻿namespace Blazee.MatLab
+{
+    public class ImageToBinary
+    {
+        public object Process(byte[] Image)
+        {
+            byte[] image = System.IO.File.ReadAllBytes(@"C:\Users\Dell\source\repos\matlabLibTest\matlabLibTest\inz.jpg");
+            var imgToProcess = new imageToBinaryNative.MLImageProcessingService();
+            var processedImage = imgToProcess.imageToBinary(image, 0.5);
+            return processedImage;
+        }
+    }
+}
