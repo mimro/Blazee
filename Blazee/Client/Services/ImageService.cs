7d7b0823ca22702d1d49d97e47035c35a4ec92ad
Michał Mroczek <micmro1@gmail.com>
2022-03-15 23:00:05 +0100

    add image processing

---
+++ Blazee/Client/Services/ImageService.cs
@@ -0,0 +1,37 @@
+﻿using Blazee.Shared.Models;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace Blazee.Client.Services
+{
+    public class ImageService
+    {
+        public byte[]? Bytes { get; set; }
+
+        public string? Type { get; set; }
+
+        //public string Base64Image => Convert.ToBase64String(Bytes);
+        //public string Source => $"data:{Type};base64,{Base64Image}";
+
+        public async Task PrepareImageFromFile(IBrowserFile? file, string ImageOutputType = "image/png")
+        {
+           // var img = await file.RequestImageFileAsync("image/png", 600, 600);
+            if (file == null)
+            {
+                throw new ArgumentNullException();
+            }
+            
+            var buffer = new byte[file.Size];
+             await file.OpenReadStream().ReadAsync(buffer);
+
+
+            Bytes = buffer;
+            Type = ImageOutputType ;
+        }
+
+
+        public async Task<ImageModel> ResizeImage(int ImageHeight, int ImageWidth)
+        {
+            return null;
+        }
+    }
+}
---
+++ Blazee/Shared/Models/ImageModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blazee.Shared.Models
+{
+    public class ImageModel
+    {
+        public string ImageBase64String { get; set; }
+
+        public string Type { get; set; } = "image/png";
+
+        private string? _source;
+        public string? Source { get { return string.IsNullOrEmpty(_source) ? $"data:{Type};base64,{ImageBase64String}" : _source; } set { _source = value; } }
+
+
+    }
+}
